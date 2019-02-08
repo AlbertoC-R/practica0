1c76e665fa0efeabaa16ba0d25540e8f7478f61f
AlbertoC-R <jose.carrillo97@uabc.edu.mx>
2019-02-08 10:09:45 -0800

    actualizacion

--- practica0/src/prueba/Prueba.java
+++ practica0/src/prueba/Prueba.java
@@ -6,7 +6,9 @@ public class Prueba {
 		// TODO Auto-generated method stub
 		p0.per p= new p0.per(20,"Luis");
 		pack2.pers m= new pack2.pers(25,"Ana","Femenino");
-		
+		p0.per p1 = new p0.per(15,"nor");
+		System.out.println(p.toString());
+		System.out.println(m.toString());
 
 	}
 
